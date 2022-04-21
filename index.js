52cc7b064f838e1395e9808f6536abaf9c1dec6c
Brooke Love <savienloveb@gmail.com>
2022-04-20 19:03:40 -0700

    adding in pseudocode

---
+++ index.js
@@ -0,0 +1,16 @@
+/* PSEUDOCODE 
+1. download inquirer
+    npm init -y
+    npm install inquirer
+2. create a template read me 
+    A. includes the all the criteria to generate a read me 
+3.create the prompts
+    A. including all the prompts to these questions: 
+        a. Description, table of contents, installation, usage, liscense, contributors, test, and Questions, github username and emaiil
+        b. add a question what would you like this read me to be called
+            i. what are badges 
+5. create afunction to split the read me
+6. createa new file with prompts in there 
+    A. declare a fs
+    write the function in a designated file for the generated read me 
+*/
